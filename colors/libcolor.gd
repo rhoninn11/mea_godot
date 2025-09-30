c19d8df0039c8de7a3a2c78365880a3af807c669
Leszek <rhoninn11@gmail.com>
2025-09-30 21:47:35 +0200

    - lerp in ok space

--- colors/libcolor.gd
+++ colors/libcolor.gd
@@ -1,21 +1,39 @@
 extends Node
 
 class Utils:
-    static func gradient(hue_a: float, hue_b: float, num: int) -> PackedFloat32Array:
-        var data: PackedFloat32Array
-        data.resize(num * 4)
+	static func color_data(color_arr: Array[Color]) -> PackedFloat32Array:
+		var num := len(color_arr)
+		var data: PackedFloat32Array
+		data.resize(num * 4)
+		for i in range(num):
+			var blend := color_arr[i];
+			data[4*i] = blend.r
+			data[4*i + 1] = blend.g
+			data[4*i + 2] = blend.b
+			data[4*i + 3] = blend.a
+		
+		return data
 
-        var color_a: Color = Color.from_ok_hsl(hue_a, 0.62, 0.2)
-        var color_b: Color = Color.from_ok_hsl(hue_b, 0.62, 0.2)
+	static func gradient(hue_a: float, hue_b: float, num: int) -> PackedFloat32Array:
+		var color_a: Color = Color.from_ok_hsl(hue_a, 0.62, 0.2)
+		var color_b: Color = Color.from_ok_hsl(hue_b, 0.62, 0.2)
+		
+		var col_arr: Array[Color];
+		col_arr.resize(num)
+		for i in range(num):
+			var prog: float = float(i)/float(num-1);
+			col_arr[i] = color_a.lerp(color_b, prog)
 
-        for i in range(num):
-            var prog: float = float(i)/float(num-1);
-            var blend = color_a.lerp(color_b, prog)
-            print(blend, "with prog: ", prog)
-            data[4*i] = blend.r
-            data[4*i + 1] = blend.g
-            data[4*i + 2] = blend.b
-            data[4*i + 3] = blend.a
+		# data.fill(0.1)
+		return color_data(col_arr)
 
-        # data.fill(0.1)
-        return data
+	
+	static func ok_gradient(ok_a: Vector3, ok_b: Vector3, num: int) -> PackedFloat32Array:
+		var col_arr: Array[Color];
+		col_arr.resize(num)
+		for i in range(num):
+			var prog: float = float(i)/float(num-1);
+			var ok_vals := ok_a.lerp(ok_b, prog);
+			col_arr[i] = Color.from_ok_hsl(ok_vals.x, ok_vals.y, ok_vals.z)
+
+		return color_data(col_arr)
--- geo/multi_mesh_instance_3d.gd
+++ geo/multi_mesh_instance_3d.gd
@@ -2,10 +2,12 @@
 extends MultiMeshInstance3D
 
 @export_tool_button("regenerate") var btn = regenerate
+@export_tool_button("regenerate_ok") var btn_ok = regenerate_ok
 
 @export var num: int = 8;
 @export var mm: MultiMesh = null;
 @export var mesh_src: Mesh;
+@export var fill: float = 0.5;
 
 func merge_instance_data(transforms: PackedFloat32Array, colors: PackedFloat32Array) -> PackedFloat32Array:
 	var n = num
@@ -25,19 +27,29 @@ func merge_instance_data(transforms: PackedFloat32Array, colors: PackedFloat32Ar
 
 	return shared_data
 
-
 func regenerate():
+	var color_data := Libcolor.Utils.gradient(0.1, 0.6, num)
+	regenerate_with_color(color_data)
+	print("+++ regenerate called")
+
+func regenerate_ok():
+	var ok_0 := Vector3(0.1, 0.62, 0.2)
+	var ok_1 := Vector3(0.6, 0.62, 0.2)
+	var color_data := Libcolor.Utils.ok_gradient(ok_0, ok_1, num)
+	regenerate_with_color(color_data)
+	print("+++ ok regeneratge called")
+
+func regenerate_with_color(color_data: PackedFloat32Array):
 	assert(mesh_src);
 	if not mm:
 		mm = MultiMesh.new()
 
-	var spots = Libgeo.Shapes.circle_4d(num, 0.5, false);
+	var spots = Libgeo.Shapes.circle_4d(num, fill, false);
 	spots = Libgeo.Math.ts_xform_orgin(spots, Libgeo.Math.scale_m(5));
 	# spots = Libgeo.Math.sin_along(spots, Vector3.UP);
 	
 	# wychodni na to, że jak będę chciał używać dodatkowych danych to trzeba je przeplatać z transformem
 	var transform_data := Libgeo.Interop.Ts2Fs(spots);
-	var color_data := Libcolor.Utils.gradient(0.1, 0.6, num);
 	# print("transform data: ",len(transform_data))
 	# print("color data: ", len(color_data))
 	var shared_data = merge_instance_data(transform_data, color_data)
